7939d6a8e53266fbae986305f1914ed1239aa599
Ellen Engdahl <ellen.engdahl@gmail.com>
2012-11-19 13:16:33 +0100

    fixed null pointer

---
+++ src/macgyvers/mds05/ServerRunner.java
@@ -0,0 +1,15 @@
+package macgyvers.mds05;
+
+import java.io.*;
+ //a class to hold at run the server listening to the socket rdy to communicate via UDP.
+public class ServerRunner {
+    public static void main(String[] args) throws IOException {
+    	new UDPServer().start();
+    	System.out.println("Server Started.");
+    	//this starts the thread that executes jobs that are in the queue.
+    	TaskHandler.getInstance().startExecuteService();
+    	System.out.println("Execute service started");
+        
+    }
+}
+
